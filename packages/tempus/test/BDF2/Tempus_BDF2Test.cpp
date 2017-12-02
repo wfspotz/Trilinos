6b9fe21e75bba223038bca50e0ae2b16a62d61eb
Irina K. Tezaur <ikalash@sandia.gov>
2017-12-01 16:24:24 -0800

    Tempus: modifying BDF2 SinCos test case to take in nTimeStepSizes (for convergence study) and initial time step size from input file, to facilitate testing.

--- packages/tempus/test/BDF2/Tempus_BDF2Test.cpp
+++ packages/tempus/test/BDF2/Tempus_BDF2Test.cpp
@@ -101,22 +101,29 @@ TEUCHOS_UNIT_TEST(BDF2, SinCos)
 {
   std::vector<double> StepSize;
   std::vector<double> ErrorNorm;
-  const int nTimeStepSizes = 7;
-  double dt = 0.2;
+    
+  // Read params from .xml file
+  RCP<ParameterList> pList =
+    getParametersFromXmlFile("Tempus_BDF2_SinCos.xml");
+
+  //Set initial time step = 2*dt specified in input file (for convergence study)
+  //
+  RCP<ParameterList> pl = sublist(pList, "Tempus", true);
+  double dt = pl->sublist("Default Integrator")
+       .sublist("Time Step Control").get<double>("Initial Time Step");
+  dt *= 2.0;
+
+  // Setup the SinCosModel
+  RCP<ParameterList> scm_pl = sublist(pList, "SinCosModel", true);
+  const int nTimeStepSizes = scm_pl->get<int>("Number of Time Step Sizes", 7); 
+
   double order = 0.0;
   for (int n=0; n<nTimeStepSizes; n++) {
-
-    // Read params from .xml file
-    RCP<ParameterList> pList =
-      getParametersFromXmlFile("Tempus_BDF2_SinCos.xml");
 
     //std::ofstream ftmp("PL.txt");
     //pList->print(ftmp);
     //ftmp.close();
 
-    // Setup the SinCosModel
-    RCP<ParameterList> scm_pl = sublist(pList, "SinCosModel", true);
-    //RCP<SinCosModel<double> > model = sineCosineModel(scm_pl);
     RCP<SinCosModel<double> > model =
       Teuchos::rcp(new SinCosModel<double>(scm_pl));
 
@@ -181,14 +188,16 @@ TEUCHOS_UNIT_TEST(BDF2, SinCos)
     ErrorNorm.push_back(L2norm);
   }
 
-  // Check the order and intercept
-  double slope = computeLinearRegressionLogLog<double>(StepSize, ErrorNorm);
-  std::cout << "  Stepper = BDF2" << std::endl;
-  std::cout << "  =========================" << std::endl;
-  std::cout << "  Expected order: " << order << std::endl;
-  std::cout << "  Observed order: " << slope << std::endl;
-  std::cout << "  =========================" << std::endl;
-  TEST_FLOATING_EQUALITY( slope, order, 0.01 );
+  if (nTimeStepSizes > 1) {
+    // Check the order and intercept
+    double slope = computeLinearRegressionLogLog<double>(StepSize, ErrorNorm);
+    std::cout << "  Stepper = BDF2" << std::endl;
+    std::cout << "  =========================" << std::endl;
+    std::cout << "  Expected order: " << order << std::endl;
+    std::cout << "  Observed order: " << slope << std::endl;
+    std::cout << "  =========================" << std::endl;
+    TEST_FLOATING_EQUALITY( slope, order, 0.01 );
+  }
   //IKT, FIXME: check what the following should be for BDF2 and reactivate! 
   //TEST_FLOATING_EQUALITY( ErrorNorm[0], 0.0486418, 1.0e-4 );
 
